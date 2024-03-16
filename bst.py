527c3986d45e9f8142f7f6b833fea4a33420fdd7
Mike Jacks <mjacks4816@xmail.dixietech.edu>
2024-03-16 09:22:28 -0600

    add subtracting from size in the delete method

--- bst.py
+++ bst.py
@@ -157,8 +157,10 @@ class NodeTree:
         
     def delete(self, value: int):
         serialized_bst_str = self.serialize()
-        serialized_bst_str = ",".join(map(lambda x: x.strip(","), serialized_bst_str.split(str(value)))).strip(",")
-        self.root = self.deserialize(serialized_bst_str).root
+        if value in [value for value in serialized_bst_str.split(",")]: 
+            serialized_bst_str = ",".join(map(lambda x: x.strip(","), serialized_bst_str.split(str(value)))).strip(",")
+            self.root = self.deserialize(serialized_bst_str).root
+            self.size -= 1
     
     # Recreated my balance tree function at home after working on it with Gabe on his BST project earlier in the day
     
